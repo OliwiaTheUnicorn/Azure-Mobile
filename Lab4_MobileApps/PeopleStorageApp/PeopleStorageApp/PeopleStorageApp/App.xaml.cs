e919b10afdfa2efaae1eb27351aa0263e090c3f1
OliwiaTheUnicorn <oliwia.katarina.rozek@gmail.com>
2021-04-15 21:39:16 +0200

    Add Lab4 MobileApps
    Signed-off-by: OliwiaTheUnicorn <oliwia.katarina.rozek@gmail.com>

---
+++ Lab4_MobileApps/PeopleStorageApp/PeopleStorageApp/PeopleStorageApp/App.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+using PeopleStorageApp.DataContracts;
+
+namespace PeopleStorageApp
+{
+    public partial class App : Application
+    {
+        private const String API_URL = "http://192.168.179.1:5000/api";
+
+        public App()
+        {
+            var client = RestEase.RestClient.For<IPeopleClient>(API_URL);
+            InitializeComponent();
+
+            MainPage = new MainPage(client);
+        }
+
+        protected override void OnStart()
+        {
+        }
+
+        protected override void OnSleep()
+        {
+        }
+
+        protected override void OnResume()
+        {
+        }
+    }
+}
---
+++ Lab4_MobileApps/PeopleStorageApp/PeopleStorageApp/PeopleStorageApp/MainViewModel.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Text;
+
+namespace PeopleStorageApp
+{
+    public class MainViewModel : INotifyPropertyChanged
+    {
+        public string UserDetails => $"First Name: {FirstName} \n Last Name: {LastName} \n Phone Number: {PhoneNumber}";
+        string firstName = string.Empty;
+        string lastName = string.Empty;
+        string phoneNumber = string.Empty;
+        public string FirstName
+        {
+            get => firstName;
+            set
+            {
+                if (firstName == value)
+                {
+                    return;
+                } else
+                {
+                    firstName = value;
+                    onPropertyChanged(nameof(FirstName));
+                    onPropertyChanged(nameof(UserDetails));
+                }
+            }
+        }
+
+        public string LastName
+        {
+            get => lastName;
+            set
+            {
+                if (lastName == value)
+                {
+                    return;
+                }
+                else
+                {
+                    lastName = value;
+                    onPropertyChanged(nameof(LastName));
+                    onPropertyChanged(nameof(UserDetails));
+                }
+            }
+        }
+
+        public string PhoneNumber
+        {
+            get => phoneNumber;
+            set
+            {
+                if (phoneNumber == value)
+                {
+                    return;
+                }
+                else
+                {
+                    phoneNumber = value;
+                    onPropertyChanged(nameof(PhoneNumber));
+                    onPropertyChanged(nameof(UserDetails));
+                }
+            }
+        }
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        void onPropertyChanged(string name)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(name));
+        }
+    }
+}
